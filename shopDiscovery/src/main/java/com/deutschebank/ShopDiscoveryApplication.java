d7548e454f97003a32b7c56ca3b756a463d79179
Saurabh <sagar.saurabh26@gmail.com>
2016-12-25 15:33:54 +0530

    initial setup

---
+++ shopDiscovery/src/main/java/com/deutschebank/ShopDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.deutschebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDiscoveryApplication.class, args);
+	}
+}
